af51624ac3b788b688dc1b0af1198fdd577b9c34
Priyanshu Dubey <dubeypriyanshu0009@gmail.com>
2024-12-29 13:25:29 +0530

    appjsx.config

--- 03reactvite/src/App.jsx
+++ 03reactvite/src/App.jsx
@@ -22,3 +22,21 @@ function App() {
 }
 
 export default App
+
+// Definition: Props (short for properties) are used to pass data from a parent component to a child component.
+// Read-Only: Props are immutable and cannot be modified by the child component.
+// Unidirectional Data Flow: Data flows from parent to child only.
+// Customization: Enables dynamic and reusable components by passing different values.
+// Access: Props are accessed in the child component via the props object.
+
+
+
+
+// React States: Key Points
+// Definition: State is an object that stores a component's dynamic data and determines its behavior.
+// Mutable: State can be updated using setState (class components) or useState (functional components).
+// Triggers Re-rendering: Updating state causes React to re-render the component.
+// Private: State is local to a component and not accessible by others unless passed via props.
+// Managing State
+// Class Components: Use this.state and update with this.setState.
+// Functional Components: Use the useState hook.
